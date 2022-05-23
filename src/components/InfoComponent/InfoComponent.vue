325e5b897c2c1b2e339f768bad35d39a920f1539
Joseff361 <Joseff361@outlook.com>
2022-05-23 18:36:50 -0500

    Implement info component

--- src/components/InfoComponent/InfoComponent.vue
+++ src/components/InfoComponent/InfoComponent.vue
@@ -1,5 +1,14 @@
 <template>
-  <section>info</section>
+  <section class="info">
+    <div class="info__header">
+      <span> Monthly Rainfall </span>
+      <span> This year </span>
+    </div>
+    <div class="info__footer">
+      <strong>22mm</strong>
+      <span>+2%</span>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -8,3 +17,7 @@ export default Vue.extend({
   name: 'InfoComponent',
 });
 </script>
+
+<style lang="scss">
+@import './InfoComponent.scss';
+</style>
