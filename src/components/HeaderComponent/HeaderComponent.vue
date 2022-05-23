7aa7dfda856e463c3b5a0537d95e3429ef4fac46
Joseff361 <Joseff361@outlook.com>
2022-05-23 17:50:14 -0500

    Implement header component

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -1,5 +1,23 @@
 <template>
-  <header>header</header>
+  <header class="header">
+    <div class="header__searcher">
+      <span class="header__searcher__title">Weather</span>
+      <span class="header__searcher__input">
+        <input type="text" placeholder="Seach Your City" />
+      </span>
+    </div>
+    <div class="header__banner">
+      <div class="header__banner__left">
+        <i class="fa-regular fa-sun"></i>
+        <strong>21º</strong>
+        <span>Mumbai, India</span>
+      </div>
+      <div class="header__banner__right">
+        <span>Monday</span>
+        <span>03:11 PM</span>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
@@ -8,3 +26,7 @@ export default Vue.extend({
   name: 'HeaderComponent',
 });
 </script>
+
+<style lang="scss">
+@import './HeaderComponent.scss';
+</style>
