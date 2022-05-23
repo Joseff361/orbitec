1b7bc3254f23c7960a6f5f54c71508d7db9a65db
Joseff361 <Joseff361@outlook.com>
2022-05-23 18:20:03 -0500

    Implement temperature component

--- src/components/TemperatureComponent/TemperatureComponent.vue
+++ src/components/TemperatureComponent/TemperatureComponent.vue
@@ -1,5 +1,38 @@
 <template>
-  <header>temperature</header>
+  <section class="temperature">
+    <div class="temperature__column">
+      <div class="temperature__section">
+        <img src="@/assets/humidity.png" alt="humidity" />
+        <div class="temperature__section__info">
+          <span>Humidity</span>
+          <strong>56%</strong>
+        </div>
+      </div>
+      <div class="temperature__section">
+        <img src="@/assets/wind.png" alt="humidity" />
+        <div class="temperature__section__info">
+          <span>Air Quality</span>
+          <strong>151</strong>
+        </div>
+      </div>
+    </div>
+    <div class="temperature__column">
+      <div class="temperature__section">
+        <img src="@/assets/sunrise.png" alt="humidity" />
+        <div class="temperature__section__info">
+          <span>Sunrise</span>
+          <strong>6:45 AM</strong>
+        </div>
+      </div>
+      <div class="temperature__section">
+        <img src="@/assets/sunset.png" alt="humidity" />
+        <div class="temperature__section__info">
+          <span>Sunset</span>
+          <strong>6:15 PM</strong>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -8,3 +41,7 @@ export default Vue.extend({
   name: 'TemperatureComponent',
 });
 </script>
+
+<style lang="scss">
+@import './TemperatureComponent.scss';
+</style>
